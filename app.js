9cf28039513f05f5897ab191a01280d5b97b0a7f
Chanthy Ngin <nginchanthy@gmail.com>
2018-02-28 21:41:14 -0800

    Refactor the code. Delete attributes that store the data. Store user's data in .data() method

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ $(document).ready(function(){
     $('form').submit(function(){
         var card = "<div class=\"card\">"
                         + "<h1>" + $('#firstname').val() + " " + $('#lastname').val() + "</h1>"
-                        + "<p data-desc=\"" + $('#desc').val() + "\""
-                        + " data-firstname=\"" + $('#firstname').val() + "\"" 
-                        + " data-lastname=\"" + $('#lastname').val() + "\"" 
-                        + " data-isslip=\"" + 0 + "\"" 
-                        + ">Click for description!</p>" 
+                        + "<p>Click for description!</p>" 
                         + "</div>";
         $('.displays').append(card);
+        // Assign the last child card to a variable lastcard
+        var lastcard = $(".displays .card:last-child");
+        //Store the user data into .data() ---> For more information jQuery .data() 
+        //https://api.jquery.com/data/
+        lastcard.data("user", {
+                    firstname: $('#firstname').val(),
+                    lastname: $('#lastname').val(),
+                    desc: $('#desc').val()
+                    });
+        lastcard.data("isslip", false);
         // Attach event listener to only the last child / the newly added card
-        slipCardHandler($(".displays .card:last-child"));
+        slipCardHandler(lastcard);
         clearForm(); // Clear all values of the form to empty
         return false;
     });
@@ -18,31 +24,18 @@ $(document).ready(function(){
 
 function slipCardHandler(selector){
     $(selector).click(function(){
-        if ($(this).children("p").attr("data-isslip") === "1") {
-            $(this).html(""
-                    + "<h1>" + $(this).children("p").attr("data-firstname") + " " + $(this).children("p").attr("data-lastname")  + "</h1>"
-                    + "<p data-desc=\"" + $(this).children("p").attr("data-desc") + "\""
-                    + " data-firstname=\"" + $(this).children("p").attr("data-firstname") + "\"" 
-                    + " data-lastname=\"" + $(this).children("p").attr("data-lastname") + "\"" 
-                    + " data-isslip=\"" + 0 + "\"" 
-                    + ">" 
-                    + "Click for description!"
-                    + "</p>"
-                );
+        if ($(this).data("isslip")) {
+            //Call back the data
+            $(this).html("<h1>" + $(this).data("user").firstname + " " + $(this).data("user").lastname  + "</h1>"
+                    + "<p>Click for description!</p>"
+            );
+            $(this).data("isslip", false);
         } else {
-            $(this).html(""
-                    + "<p data-desc=\"" + $(this).children("p").attr("data-desc") + "\""
-                    + " data-firstname=\"" + $(this).children("p").attr("data-firstname") + "\"" 
-                    + " data-lastname=\"" + $(this).children("p").attr("data-lastname") + "\"" 
-                    + " data-isslip=\"" + 1 + "\"" 
-                    + ">" 
-                    + $(this).children("p").attr("data-desc")
-                    + "</p>"
-                );
+            $(this).html("<p>" + $(this).data("user").desc + "</p>");
+            $(this).data("isslip", true);
         }
     });
 }
-
 
 function clearForm(){
     $('#firstname').val("");
